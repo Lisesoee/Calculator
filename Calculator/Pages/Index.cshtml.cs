5010191041978efb474412c4b363e25d9833fadd
Lise Jacobsen <lise.jacobsen@hansencx.com>
2023-10-04 11:06:07 +0200

    Split calculation logic into calculator service

--- Calculator/Pages/Index.cshtml.cs
+++ Calculator/Pages/Index.cshtml.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Calculator.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Calculator.Pages
 {
     public class IndexModel : PageModel
-    {
+    {       
+
+        public double Result { get; set; }
+
+        public void OnPostAdd(double num1, double num2)
+        {
+            var myCalculator = new CalculatorService();
+            Result = myCalculator.Add(num1, num2);
+        }
+
+        public void OnPostSubtract(double num1, double num2)
+        {
+            var myCalculator = new CalculatorService();
+            Result = myCalculator.Subtract(num1, num2);
+        }
+
         //private readonly ILogger<IndexModel> _logger;
 
         //public IndexModel(ILogger<IndexModel> logger)
@@ -16,17 +32,17 @@ namespace Calculator.Pages
         //{
 
         //}
-        public IActionResult OnPostAdd(double num1, double num2)
-        {
-            double result = num1 + num2;
-            return new JsonResult(result);
-        }
+        //public IActionResult OnPostAdd(double num1, double num2)
+        //{
+        //    double result = num1 + num2;
+        //    return new JsonResult(result);
+        //}
 
-        public IActionResult OnPostSubtract(double num1, double num2)
-        {
-            double result = num1 - num2;
-            return new JsonResult(result);
-        }
+        //public IActionResult OnPostSubtract(double num1, double num2)
+        //{
+        //    double result = num1 - num2;
+        //    return new JsonResult(result);
+        //}
 
         //public async Task<IActionResult> OnPostAdd(double num1, double num2)
         //{
