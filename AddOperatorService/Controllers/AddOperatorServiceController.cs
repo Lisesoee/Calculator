bbc3d3ad00fd6edcad8a3394e0c83c7de90520c5
Lise Jacobsen <lise.jacobsen@hansencx.com>
2023-10-04 15:32:33 +0200

    Create and insert operations into database
    Create a seperate datebase service for AddOperatorService and SubtractOperatorService. When performing an operation, it will create the table if it is missing and insert the operation performed into the table.

--- AddOperatorService/Controllers/AddOperatorServiceController.cs
+++ AddOperatorService/Controllers/AddOperatorServiceController.cs
@@ -1,4 +1,7 @@
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using System.Data;
 
 namespace AddOperatorService.Controllers
 {
@@ -6,11 +9,25 @@ namespace AddOperatorService.Controllers
     [Route("[controller]")]
     public class AddOperatorServiceController : ControllerBase
     {
+        private static IDbConnection addOperationsdb = new MySqlConnection("Server=addOperations-db;Database=addOperations-database;Uid=guest;Pwd=1234;");
+
         [HttpGet]
         public int Get(int a, int b)
         {
-            var result = a + b;
+            int result = a + b;
             Console.WriteLine(a + " + " + b + " = " + result);
+
+            addOperationsdb.Open();
+            var tables = addOperationsdb.Query<string>("SHOW TABLES LIKE 'addOperations'");
+            if (!tables.Any())
+            {
+                addOperationsdb.Execute("CREATE TABLE addOperations (id INT AUTO_INCREMENT PRIMARY KEY, a INT NOT NULL, b INT NOT NULL, result INT NOT NULL)");
+                Console.WriteLine("Table created");
+            }
+
+            addOperationsdb.Execute("INSERT INTO addOperations (a, b, result) VALUES (@a, @b, @result)", new { a = a, b = b, result = result });
+            Console.WriteLine("Result stored in database");
+            addOperationsdb.Close();           
 
             return result;
         }
--- SubtractOperatorService/Controllers/SubtractOperatorServiceController.cs
+++ SubtractOperatorService/Controllers/SubtractOperatorServiceController.cs
@@ -1,4 +1,7 @@
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using System.Data;
 
 namespace SubtractOperatorService.Controllers
 {
@@ -6,11 +9,25 @@ namespace SubtractOperatorService.Controllers
     [Route("[controller]")]
     public class SubtractOperatorServiceController : ControllerBase
     {
+        private static IDbConnection subtractOperationsdb = new MySqlConnection("Server=subtractOperations-db;Database=subtractOperations-database;Uid=guest;Pwd=1234;");
+
         [HttpGet]
         public int Get(int a, int b)
         {
             var result = a - b;
             Console.WriteLine(a + " - " + b + " = " + result);
+
+            subtractOperationsdb.Open();
+            var tables = subtractOperationsdb.Query<string>("SHOW TABLES LIKE 'subtractOperations'");
+            if (!tables.Any())
+            {
+                subtractOperationsdb.Execute("CREATE TABLE subtractOperations (id INT AUTO_INCREMENT PRIMARY KEY, a INT NOT NULL, b INT NOT NULL, result INT NOT NULL)");
+                Console.WriteLine("Table created");
+            }
+
+            subtractOperationsdb.Execute("INSERT INTO subtractOperations (a, b, result) VALUES (@a, @b, @result)", new { a = a, b = b, result = result });
+            Console.WriteLine("Result stored in database");
+            subtractOperationsdb.Close();
 
             return result;
         }
