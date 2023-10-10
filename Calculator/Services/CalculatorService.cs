eb66d6bebdb721b8706c1682f4ba7d2d84a3e891
Lise Jacobsen <lise.jacobsen@hansencx.com>
2023-10-10 10:55:18 +0200

    Added retry and fault isolation to add and subtract operations

--- Calculator/Services/CalculatorService.cs
+++ Calculator/Services/CalculatorService.cs
@@ -17,141 +17,77 @@ namespace Calculator.Services
         private static RestClient subtractOperationRestClient = new RestClient("http://subtractoperatorservice/SubtractOperatorService/");
         public async Task<double> Add(double num1, double num2)
         {
-            //var task = addOperationRestClient.GetAsync<int>(new RestRequest("/GetResult?a=" + num1 + "&b=" + num2));
-
-            //Console.WriteLine("Retrived result from Add operation: " + task.Result);
-            //return task.Result;
-
 
             var retryPolicy = Policy.Handle<Exception>()
-                .WaitAndRetryAsync(2, retryAttempt =>
+                .WaitAndRetryAsync(5, retryAttempt =>
                 {
-                    Console.WriteLine("Attempt... " + retryAttempt);
-                    var timeToRetry = TimeSpan.FromSeconds(2);
-                    Console.WriteLine($"Waiting {timeToRetry.TotalSeconds} seconds");
+                    Console.WriteLine("Retry attempt... " + retryAttempt);
+                    var timeToRetry = TimeSpan.FromSeconds(1);
+                    Console.WriteLine($"Waiting {timeToRetry.TotalSeconds} seconds before next retry");
                     return timeToRetry;
                 });
-
             try
             {
                 return await retryPolicy.ExecuteAsync(async () =>
                 {
-                    //var response = await httpClient.GetAsync("https://urlnotexist.com/api/products/1");
-                    //response.EnsureSuccessStatusCode();
-
-                    var response = await AddAsync(num1, num2);
-
-                    //return ((long)response);
-                    return response;
-                    
-
-                    Console.WriteLine("Attempting to get added result...");
-                    var task = AddAsync(num1, num2);
+                    var task = addOperationRestClient.GetAsync<int>(new RestRequest("/GetResult?a=" + num1 + "&b=" + num2));
                     await task;
-                    return task.Result;
 
-                    //var task = await AddAsync(num1, num2);
-                    //return task;//.Result;
+                    if (task?.Status == TaskStatus.RanToCompletion)
+                    {
+                        Console.WriteLine("Retrived result from Add operation: " + task.Result);
+                        return task.Result;
+                    }
+                    if (task?.Status == TaskStatus.Faulted)
+                    {
+                        throw new Exception("Request failed. Task status: " + task?.Status);
+                    }
+                    throw new Exception("Unexpected Task status: " + task?.Status);
 
-
-                    //await AddAsync(num1, num2);
-
-                    //var task = addOperationRestClient.GetAsync<int>(new RestRequest("/GetResult?a=" + num1 + "&b=" + num2));
-                    ////await task.RunSynchronously();
-
-                    //if (task?.Status == TaskStatus.RanToCompletion)
-                    //{
-                    //    Console.WriteLine("Retrived result from Add operation: " + task.Result);
-                    //    return task.Result;
-                    //}
-
-                    //throw new Exception("Request failed. Task status: " + task?.Status);
                 });
             }
             catch (Exception ex)
-            {
-                
-                //Console.WriteLine("Final Throw");     
-                Console.WriteLine("Finbal exeption: " + ex.ToString());
-                return 8888888888;
+            {  
+                Console.WriteLine("Ran out of retries. Final exception: " + ex.ToString());
+                return 0;
             }
-
-
-            // This code is to avoid "not all code paths return a value", but is not meant to actually be called...
-            return 999999999;
-
-            //await AddAsync(num1, num2);
-            //var task = AddAsync(num1, num2);
-            //return task.Result;
-
-            //var task = addOperationRestClient.GetAsync<int>(new RestRequest("/GetResult?a=" + num1 + "&b=" + num2));
-            //if (task?.Status == TaskStatus.RanToCompletion)
-            //{
-            //    Console.WriteLine("Retrived result from Add operation: " + task.Result);
-            //    return task.Result;
-            //}
-            //throw new Exception("Request failed. Task status: " + task?.Status);
-
-
-            //policy.Execute(() =>
-            //{
-            //    //var task = restClient.GetAsync<int>(new RestRequest("/cache?number=" + number));
-            //    //await task;
-
-            //    Console.WriteLine("Attempting to get added result...");
-            //    var task = addOperationRestClient.GetAsync<int>(new RestRequest("/GetResult?a=" + num1 + "&b=" + num2));
-
-
-            //    if (task?.Status == TaskStatus.RanToCompletion)
-            //    {
-            //        Console.WriteLine("Retrived result from Add operation: " + task.Result);
-            //        return task.Result;
-            //    }
-
-            //    throw new Exception("Request failed. Task status: " + task?.Status);
-            //});
-
-            //var task = addOperationRestClient.GetAsync<int>(new RestRequest("/GetResult?a=" + num1 + "&b=" + num2));
-            //return task.Result;
-            //return 0;
-
-            //});
-
-        }
-
-        public async Task<double> AddAsync(double num1, double num2)
-        {
-            //var task = addOperationRestClient.GetAsync<int>(new RestRequest("/GetResult?a=" + num1 + "&b=" + num2));
-
-            //Console.WriteLine("Retrived result from Add operation: " + task.Result);
-            //return task.Result;
-
-            var task = addOperationRestClient.GetAsync<int>(new RestRequest("/GetResult?a=" + num1 + "&b=" + num2));
-            await task;
-            //task.Wait();
-
-            if (task?.Status == TaskStatus.RanToCompletion)
-            {
-                Console.WriteLine("Retrived result from Add operation: " + task.Result);
-                return task.Result;
-            }
-
-            if (task?.Status == TaskStatus.Faulted)
-            {
-                throw new Exception("Request failed. Task status: " + task?.Status);                
-            }
-
-            Console.WriteLine("Task status: " + task?.Status);
-
-            return 7777777777;
         }
 
         public async Task<double> Subtract(double num1, double num2)
         {
-            var task = subtractOperationRestClient.GetAsync<int>(new RestRequest("/GetResult?a=" + num1 + "&b=" + num2));
+            var retryPolicy = Policy.Handle<Exception>()
+                .WaitAndRetryAsync(5, retryAttempt =>
+                {
+                    Console.WriteLine("Retry attempt... " + retryAttempt);
+                    var timeToRetry = TimeSpan.FromSeconds(1);
+                    Console.WriteLine($"Waiting {timeToRetry.TotalSeconds} seconds before next retry");
+                    return timeToRetry;
+                });
+            try
+            {
+                return await retryPolicy.ExecuteAsync(async () =>
+                {
+                    var task = subtractOperationRestClient.GetAsync<int>(new RestRequest("/GetResult?a=" + num1 + "&b=" + num2));
+                    await task;
 
-            Console.WriteLine("Retrived result from Subtract operation: " + task.Result);
-            return task.Result;
+                    if (task?.Status == TaskStatus.RanToCompletion)
+                    {
+                        Console.WriteLine("Retrived result from Subtract operation: " + task.Result);
+                        return task.Result;
+                    }
+                    if (task?.Status == TaskStatus.Faulted)
+                    {
+                        throw new Exception("Request failed. Task status: " + task?.Status);
+                    }
+                    throw new Exception("Unexpected Task status: " + task?.Status);
+
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ran out of retries. Final exception: " + ex.ToString());
+                return 0;
+            }
         }
 
         public async Task<List<MathematicalOpearation>> GetListOfItemsAsync()
@@ -164,8 +100,6 @@ namespace Calculator.Services
             var operationList = new List<MathematicalOpearation>();
             operationList.AddRange(addOperationList);
             operationList.AddRange(subtractOperationList);
-
-            //operationList.Sort();
 
             return operationList;
         }
